034a4469b4354f14f27211b7f04c20461b4cd894
Ifeel <821314629@qq.com>
2021-10-11 16:47:25 +0800

    Create 10.lua

---
+++ 10/10.lua
@@ -0,0 +1,9 @@
+﻿
+
+
+--练习10.1：请编写一个函数split，该函数接收两个参数，第1个参数是字符串，第2个参数是分隔符模式，
+--函数的返回值是分隔符分割后的原始字符串中的每一部分的序列：
+--t = split("a whole new world", " ")
+--t = {"a", "whole", "new", "world")
+--你编写的函数是如何处理空字符串的呢？特别是，一个空字符串究竟是空序列(an empty sequence)，
+--还是一个具有字符串的序列呢（a sequence with one empty string）？
